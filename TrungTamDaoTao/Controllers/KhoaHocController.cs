cad614ec5ef9268dc2fef1c9522496b1133df580
dungbaoviec123 <dungtran160820051508@gmail.com>
2025-04-10 16:46:13 +0700

    add CRUD KhoaHoc

--- TrungTamDaoTao/Controllers/KhoaHocController.cs
+++ TrungTamDaoTao/Controllers/KhoaHocController.cs
@@ -16,5 +16,86 @@ namespace TrungTamDaoTao.Controllers
             IEnumerable<KhoaHoc> khoaHocList = _context.KhoaHocs;
             return View(khoaHocList);
         }
+
+        //GET: KhoaHoc/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST: KhoaHoc/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(KhoaHoc obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.KhoaHocs.Add(obj);
+                _context.SaveChanges();
+                TempData["success"] = "Thêm khóa học thành công";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //GET: KhoaHoc/Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var khoaHocFromDb = _context.KhoaHocs.Find(id);
+            if (khoaHocFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(khoaHocFromDb);
+        }
+        //POST: KhoaHoc/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(KhoaHoc obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.KhoaHocs.Update(obj);
+                _context.SaveChanges();
+                TempData["success"] = "Cập nhật khóa học thành công";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //GET: KhoaHoc/Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var khoaHocFromDb = _context.KhoaHocs.Find(id);
+            if (khoaHocFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(khoaHocFromDb);
+        }
+        //POST: KhoaHoc/Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(KhoaHoc obj)
+        {
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            var khoaHocFromDb = _context.KhoaHocs.Find(obj.MaKhoaHoc);
+            if (khoaHocFromDb == null)
+            {
+                return NotFound();
+            }
+            _context.KhoaHocs.Remove(khoaHocFromDb);
+            _context.SaveChanges();
+            TempData["success"] = "Xóa khóa học thành công";
+            return RedirectToAction("Index");
+        }
     }
 }
--- TrungTamDaoTao/Models/KhoaHoc.cs
+++ TrungTamDaoTao/Models/KhoaHoc.cs
@@ -6,11 +6,18 @@ namespace TrungTamDaoTao.Models
     public class KhoaHoc
     {
         [Key]
+        [Display(Name = "Mã khóa học")]
         public int MaKhoaHoc { get; set; }    // Khóa chính (ID khóa học)
+
+        [Display(Name = "Tên khoá học")]
         public string TenKhoaHoc { get; set; }  // Tên khóa học
+        [Display(Name = "Tên giảng viên")]
         public string GiangVien { get; set; }  // Giảng viên
+        [Display(Name = "Thời gian khai giảng")]
         public DateTime ThoiGianKhaiGiang { get; set; }  // Thời gian khai giảng
+        [Display(Name = "Học phí")]
         public decimal HocPhi { get; set; }   // Học phí
+        [Display(Name = "Số lượng học viên")]
         public int SoLuongHocVien { get; set; } // Số lượng học viên
 
         public ICollection<DangKyKhoaHoc> DangKyKhoaHocs { get; set; }
